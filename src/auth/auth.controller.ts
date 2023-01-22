b97341341d8d7016bdcc47a619e6cae922234b8d
LeonMesquita <leonmesquita16@gmail.com>
2023-01-22 18:09:10 -0300

    docs: add Swagger documentation for auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,14 +7,29 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { User } from './entities/user.entity';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
+  @ApiOperation({ summary: 'Criar um novo usuário' })
+  @ApiResponse({
+    status: 201,
+    description: 'Usuário criado com sucesso',
+    type: User,
+  })
+  @ApiResponse({ status: 400, description: 'Parâmetros inválidos' })
+  @ApiResponse({
+    status: 409,
+    description: 'Email já existente',
+  })
   signUp(@Body() CreateUserDto: CreateUserDto) {
     return this.authService.create(CreateUserDto);
   }
@@ -22,7 +37,13 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @Post('sign-in')
   @HttpCode(200)
-  signIn(@Req() req: any) {
+  @ApiOperation({ summary: 'Realizar login de usuário' })
+  @ApiResponse({
+    status: 200,
+    description: 'Usuário logado com sucesso',
+  })
+  @ApiResponse({ status: 401, description: 'Email ou senha incorretos' })
+  signIn(@Req() req: any, @Body() loginUserDto: LoginUserDto) {
     return this.authService.signIn(req.user);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Event Management API')
+    .setDescription(
+      'Documentação Swagger de uma API de gerenciamento de eventos',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
